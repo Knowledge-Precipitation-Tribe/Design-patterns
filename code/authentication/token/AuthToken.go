0c7f2b391103d0f59014c60f614f1af274462461
super <supeng842499467@gmail.com>
2020-06-03 10:22:10 +0800

    code

--- code/authentication/token/AuthToken.go
+++ code/authentication/token/AuthToken.go
@@ -1,1 +1,23 @@
 package token
+
+import "time"
+
+const EXPIRE_INTERVAL = 10 * 60 * 1000
+
+
+type AuthToken struct {
+	createTime int64
+	token string
+	originalUrl string
+}
+
+func (a AuthToken) IsExpired() bool {
+	if a.createTime > time.Now().Unix(){
+		return true
+	}
+	return false
+}
+
+func (a AuthToken) Match() bool{
+	return false
+}
